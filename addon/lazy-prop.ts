1c5799e6d85235329b788a1c3340c1cc4ecb5c81
Chris Thoburn <cthoburn@linkedin.com>
2019-04-05 10:10:50 -0700

    maybe fix ts

--- addon/lazy-prop.ts
+++ addon/lazy-prop.ts
@@ -11,7 +11,7 @@ interface Descriptor<T> extends TypedPropertyDescriptor<T> {
     get: () => any;
 }
 
-export default function lazyProp(target: any, name: string, descriptor: Descriptor<string>): TypedPropertyDescriptor<string> {
+export default function lazyProp(target: any, name: string, descriptor: Descriptor<any>): TypedPropertyDescriptor<any> {
   let key = `___${name}`;
   const get = descriptor.get;
   const set = descriptor.set;
